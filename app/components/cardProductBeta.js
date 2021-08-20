bb09bb6ec4a6edabf0d4e0fe335c6dcbee79a505
LuisEduardoLopez33 <193274@ids.upchiapas.edu.mx>
2021-08-20 12:49:00 -0500

    imagnes beta

---
+++ app/components/cardProductBeta.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import CarritoIcono from "../assets/icons/shopping_cart_black_24dp.svg";
+class cardProductBeta extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                <div className="row ">
+                    <div className="col" >
+                        <div className="card"  >
+                            <img src={this.props.img}  className="card-img"  width="60" height="220"/>
+                            <div className="card-body">
+                                <h5 className="card-title">{this.props.title}</h5>
+                                <p className="card-text">{this.props.description} </p>
+                                <button type="button" className="btn btn-white" >
+                                    <img src={CarritoIcono} alt="" width="30" height="24"/>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+
+}
+export default cardProductBeta;
--- app/pages/Home.js
+++ app/pages/Home.js
@@ -2,12 +2,49 @@ import React from 'react'
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Card from "../components/cardProductBeta";
+import img1 from "../assets/img/800_800_F4B08AE864B230306F27DB61747B438539D82EF121556B2Bmp.png";
+import img2 from "../assets/img/iphone-12-blue-select-2020.png";
+import img3 from "../assets/img/latin-galaxy-note20-ultra-n985-sm-n985fznkgto-frontmysticbronze-320814310.png";
+import img4 from "../assets/img/latin-galaxy-s21-5g-g991-sm-g991bzajtpa-368316714.png";
 
-import img from "../assets/img/pngtree-img-file-document-icon-png-image_4166554.jpg"
+import img from "../assets/img/pngtree-img-file-document-icon-png-image_4166554.jpg";
 
 class Home extends React.Component{
     constructor(props) {
         super(props);
+
+        this.state = {
+            productList:[
+            {
+               imagen : img2,
+                nombre: "Iphone 12 blue",
+               precio: "$20,500"
+
+            },
+            {
+                imagen : img1,
+                nombre: "Huawei P40 pro",
+                precio: "$25,000"
+
+            },
+            {
+                imagen : img3,
+                nombre: "Samsung Galaxy Note 20 Ultra bronze",
+                precio: "$22,000"
+
+            },
+            {
+                imagen : img4,
+                nombre: "Samsung Galaxy S21 Ultra 5g",
+                precio: "$20,500"
+
+            }
+
+        ]
+
+        }
+        this.status = false
     }
     render() {
         return(
@@ -55,7 +92,13 @@ class Home extends React.Component{
                         </div>
                     </div>
                 </div>
-
+                    <div className="row">
+                        <For each="item" index="index" of={this.state.productList} >
+                            <div className="col-sm-6 col-md-4 col-lg-4">
+                                <Card key={index} img ={item.imagen} title={item.nombre} description={item.precio} />
+                            </div>
+                        </For>
+                    </div>
                 <br/>
                 <br/>
                 <br/>
